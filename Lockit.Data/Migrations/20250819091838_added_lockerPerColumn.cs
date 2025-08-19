c8ef28a8eb6bf97565f75c9452ee3bbf6ff09b70
Chris Timmerman <chris.timmerman@vdabcampus.be>
2025-08-19 12:39:31 +0200

    Added new migration for lockerPerColumn field in Location

---
+++ Lockit.Data/Migrations/20250819091838_added_lockerPerColumn.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lockit.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_lockerPerColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LockersPerColumn",
+                table: "Locations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LockersPerColumn",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
